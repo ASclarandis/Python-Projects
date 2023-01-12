6610e4aae1ee3f5eea607511d2f9767beebefae6
Anna Sclarandis <116303239+ASclarandis@users.noreply.github.com>
2023-01-11 18:04:08 -0800

    Upload classes.py

---
+++ classes.py
@@ -0,0 +1,17 @@
+
+class User:
+    name = ''
+    email = ''
+    password = ''
+
+class Teacher(User):
+    teacher_id = 0
+    subject = ''
+
+class Student(User):
+    student_id = 0
+    major = ''
+    gpa = 0
+    
+    
+        
